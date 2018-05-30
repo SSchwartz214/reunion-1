4f7ca46bc43b307990bb311a0d1dc9ecaa7c1913
Seth Schwartz <sschwartz214@gmail.com>
2018-05-30 10:43:57 -0600

    adds location and add_activities methods with tests

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,24 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_it_has_attributes
+    reunion = Reunion.new("Miami")
+
+    assert_equal "Miami", reunion.location
+  end
+
+  def test_it_can_have_activities
+    reunion = Reunion.new("Miami")
+    activity_1 = Activity.new("Golf", 100)
+    activity_2 = Activity.new("Surfing", 50)
+
+    reunion.add_activity(activity_1)
+
+    assert_equal 1, reunion.activities.count
+    assert_equal "Golf", reunion.activities.last.name
+
+    reunion.add_activity(activity_2)
+
+    assert_equal 50, reunion.activities.last.cost
+  end
 end
